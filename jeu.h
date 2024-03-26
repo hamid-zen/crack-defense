5343447647cad7ac4d6bfb4dbddd7c008dfe8c65
graphitech <143335495+arigraphitech@users.noreply.github.com>
2024-03-27 00:16:04 +0100

    Update jeu.h

--- jeu.h
+++ jeu.h
@@ -16,7 +16,10 @@ public:
     jeu(coordonne _max_hauteur=12,coordonne _max_largeur=6,int colors=4);
     bool perdu();
     void deplacer_viseur(tdirection dir);
-      std::pair<unsigned int,unsigned int> faire_tomber(coordonne x,coordonne y ) ; //retourne endposition de la case qui glisse
+   std::pair<unsigned int,unsigned int> faire_tomber(coordonne x,coordonne y ) ; //retourne endposition de la case qui glisse
+ std::vector<std::pair<unsigned int,unsigned int>> alignement_vertical();
+    std::vector<std::pair<unsigned int,unsigned int>> alignement_horizontale();
+    std::vector<std::pair<unsigned int,unsigned int>> alignement(); //retourne l'ensemble des position qui forme le premier alignement trouvé
 
 private:
     grid _grille1;
