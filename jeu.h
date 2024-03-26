bcfd256e5b6a35bf3bbe61672f0fe0143dc1cec5
graphitech <143335495+arigraphitech@users.noreply.github.com>
2024-03-26 22:32:52 +0100

    Update jeu.h

--- jeu.h
+++ jeu.h
@@ -16,6 +16,7 @@ public:
     jeu(coordonne _max_hauteur=12,coordonne _max_largeur=6,int colors=4);
     bool perdu();
     void deplacer_viseur(tdirection dir);
+      std::pair<unsigned int,unsigned int> faire_tomber(coordonne x,coordonne y ) ; //retourne endposition de la case qui glisse
 
 private:
     grid _grille1;
