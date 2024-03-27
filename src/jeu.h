bbdf7eba5417b97842e8e99a6ce7d6faf37ea9d0
graphitech <143335495+arigraphitech@users.noreply.github.com>
2024-03-28 00:58:53 +0100

    Update jeu.h

--- src/jeu.h
+++ src/jeu.h
@@ -21,9 +21,14 @@ public:
     void deplacer_viseur(t_direction dir);
     void afficher() const;
     void echanger_cases_viseur();
+    std::pair<unsigned int, unsigned int> faire_tomber(coordonne x, coordonne y);
+    std::vector<std::pair<unsigned int, unsigned int> >  alignement_vertical();
+    std::vector<std::pair<unsigned int, unsigned int> > alignement_horizontale();
+    std::vector<std::pair<unsigned int, unsigned int> >  alignement();
     std::pair<coordonne,coordonne> getcase1viseur() const; //recupere la position de la premiere case du viseur
     std::pair<coordonne,coordonne> getcase2viseur() const;
     t_colors getColor(std::pair<coordonne,coordonne> pair) const; //recupere la couleur de la case qui a pour position pair
+    void changer_sense_viseur();
 private:
     grid _grille;
     viseur _viseur;
