2f3f4b22ee8f47b80b9daca209681079875a2854
Brachyy <157481890+Brachyy@users.noreply.github.com>
2024-04-03 16:08:59 +0200

    Delete build directory

--- build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zen/Etudes/L3/crack-defense/test
-# Build directory: /home/zen/Etudes/L3/crack-defense/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/zen/Etudes/L3/crack-defense/build/test/unit_tests_include-b12d07c.cmake")
-subdirs("../_deps/catch2-build")
