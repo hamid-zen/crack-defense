3647baaaad3bdf08e0e69f98735d2400bd64bce2
Brachyy <157481890+Brachyy@users.noreply.github.com>
2024-04-06 02:16:59 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 //TODO: tout traduire en une seule langue
 int main(){
     interface _interface;
-    _interface.play();
+    _interface.menu();
+    //_interface.play();
     return 0;
 }
--- src/interface.h
+++ src/interface.h
@@ -1,8 +1,16 @@
 #include "arbitre.h"
+#include<iostream>
+using dimension = u_int16_t;
+
 
 class interface{
 public:
     interface();
     void play();
+    void menu();
 private:
+    dimension _width;
+    t_number_color _difficulty ;
+    sf::Font _font;
+
 };
